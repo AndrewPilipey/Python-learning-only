3180aea8ba6bc265253c86ca3202180457b6120f
AndrewPilipey <andrewpilipeyone@gmail.com>
2023-04-28 22:13:16 +0300

    forming a habit

---
+++ УПЗ7Ч7.py
@@ -0,0 +1,21 @@
+#tuples (кортежи)
+
+# x = (9, 8, 7, 6, 5, 4, 3)
+# y = []
+# for i in range(len(x)):
+#     y.append(x[i] +3)
+#
+#
+# #a, b, c = x
+# r = 5
+# u = 4
+# print(r, u)
+# r, u = (u, r)
+# print(r, u)
+# print(x[1:5])
+
+x1 = (1, 2, 3, 4, 5)
+print(x1)
+t = list(x1)
+t[0]
+
