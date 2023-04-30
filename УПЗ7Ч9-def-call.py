45fdfbc6a7ea5bc864f2bdac91cefd9ad00abd2f
AndrewPilipey <andrewpilipeyone@gmail.com>
2023-04-30 20:21:49 +0300

    forming a habit

---
+++ УПЗ7Ч9-def-call.py
@@ -0,0 +1,18 @@
+#функции
+print('Before function')
+
+def show() :
+    print('Function')
+def show2() :
+    x = 7 + v
+    return x
+v = 99
+y = show2()
+z = show2() + 9
+
+print(y)
+print(z)
+show() #function call
+print('After function')
+show()
+print(show())
