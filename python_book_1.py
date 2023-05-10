600a6b2d83820152cd8b1b1c4422f5a5dccd9daf
AndrewPilipey <andrewpilipeyone@gmail.com>
2023-05-10 21:54:03 +0300

    habit formation

--- python_book_1.py
+++ python_book_1.py
@@ -1,2 +1,9 @@
-name = input("What's your name?\n")
-print("Hi,", name)
+# name = input("What's your name?\n")
+# print("Hi,", name)
+# print(type(name))
+# m = [1, 2, 3]
+# for i in m:
+#     print(i)
+a = b = 1
+print(a is b)
+print(b is a)
