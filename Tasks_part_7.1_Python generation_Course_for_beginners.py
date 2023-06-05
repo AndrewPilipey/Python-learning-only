e6ad45feb96ac9cfb36dea21783b3687136b8c2c
AndrewPilipey <andrewpilipeyone@gmail.com>
2023-06-05 22:38:24 +0300

    habit formation

---
+++ Tasks_part_7.1_Python generation_Course_for_beginners.py
@@ -0,0 +1,41 @@
+#task
+for i in range(10):
+    print("Python is awesome!")
+
+
+#next_task
+text = input()
+for _ in range(int(input())):
+    print(text)
+
+
+#next_task
+for _ in range(6):
+    print("AAA")
+for _ in range(5):
+    print("BBBB")
+print("E")
+for _ in range(9):
+    print("TTTTT")
+print("G")
+
+
+#next_task
+line = int(input())
+for _ in range(line):
+    print("*" * 19)
+
+
+#next_task
+doremi = input()
+for i in range(10):
+    print(i, doremi)
+
+
+#next_task
+num = int(input())
+for i in range(num + 1):
+    print("Квадрат числа", i, "равен", i * i)
+
+
+#next_task
